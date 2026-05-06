f5de64618a8af3d16544b24cb88c206bc284d1df
amarmeshram567 <meshramamar567@gmail.com>
2026-05-06 19:31:31 +0530

    fixed bug 2

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -3,7 +3,7 @@ import { toast } from "sonner";
 
 const API_URL = import.meta.env.DEV
   ? "/api"
-  : import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+  : import.meta.env.VITE_API_URL || "https://shoppingapp-xi.vercel.app/api";
 
 export const api = axios.create({
   baseURL: API_URL,
